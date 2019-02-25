685bfeb2e56613400de1d013b25e5a2a62620176
Faragó Kornél <fkorika97@gmail.com>
2019-02-25 21:11:49 +0100

    BaseMap,BaseArts,CameraScript,SortingLayout

---
+++ 2D RPG ONLAB/Assets/Scripts/Player_Heroes/Player2D.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2D : MonoBehaviour
+{
+    
+    Rigidbody2D rigidbody;
+    Vector2 velocity;
+    public float m_MovementSpeed;
+    public int m_PlayerID = 0;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Quaternion q = transform.rotation;
+        q.eulerAngles = new Vector3(q.eulerAngles.x, q.eulerAngles.y, 0);
+        transform.rotation = q;
+
+        if (m_PlayerID == 0)
+            Debug.Log("You forgot to give your players ID-s");
+        else
+            velocity = new Vector2(Input.GetAxisRaw("Horizontal" + m_PlayerID), Input.GetAxisRaw("Vertical" + m_PlayerID)).normalized * m_MovementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
