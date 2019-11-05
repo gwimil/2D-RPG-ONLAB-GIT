c86fd1056ee1a360380ec03d87cdf6b31822528f
Gwimil <fkorika97@gmail.com>
2019-11-05 17:42:33 +0100

    push3

--- 2D RPG ONLAB/Assets/Scripts/Mobs/Fireball4Ways.cs
+++ 2D RPG ONLAB/Assets/Scripts/Mobs/Fireball4Ways.cs
@@ -2,17 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fireball4Ways : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace EventCallbacks
+{
+    public class Fireball4Ways : MonoBehaviour
     {
+        public Projectile m_EnemyFireBall;
+
+        private bool attack;
+
+        private Vector2 vectorMultiplier;
+        private Vector2 firstAngle;
+        private Vector2 secondAngle;
+        private Vector2 thirdAngle;
+        private Vector2 fourthAngle;
+
+        public float rotateSpeed;
+        public int rotateDirection; // 1 or -1
         
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rotateSpeed = 5;
+            rotateDirection = 1;
+            attack = true;
+            vectorMultiplier = new Vector2(0.1f, 0.1f);
+            firstAngle = new Vector2(1, 0);
+            secondAngle = new Vector2(0, 1);
+            thirdAngle = new Vector2(-1, 0);
+            fourthAngle = new Vector2(0, -1);
+            InvokeRepeating("LaunchProjectile", 2.0f, 0.3f);
+        }
+
+
+        public void startAttacking(bool b)
+        {
+            attack = b;
+        }
+
+        void LaunchProjectile()
+        {
+            if (attack)
+            {
+                Projectile fb1 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
+                fb1.setDirection(firstAngle);
+
+                Projectile fb2 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
+                fb2.setDirection(secondAngle);
+
+                Projectile fb3 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
+                fb3.setDirection(thirdAngle);
+
+                Projectile fb4 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
+                fb4.setDirection(fourthAngle);
+
+                firstAngle = RotateVector2(firstAngle, rotateSpeed * rotateDirection);
+                secondAngle = RotateVector2(secondAngle, rotateSpeed * rotateDirection);
+                thirdAngle = RotateVector2(thirdAngle, rotateSpeed * rotateDirection);
+                fourthAngle = RotateVector2(fourthAngle, rotateSpeed * rotateDirection);
+
+            }
+            else
+            {
+                firstAngle = new Vector2(1, 0);
+                secondAngle = new Vector2(0, 1);
+                thirdAngle = new Vector2(-1, 0);
+                fourthAngle = new Vector2(0, -1);
+            }
+        }
+
+        private Vector2 RotateVector2(Vector2 v, float degrees)
+        {
+            float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+            float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+            float tx = v.x;
+            float ty = v.y;
+            v.x = (cos * tx) - (sin * ty);
+            v.y = (sin * tx) + (cos * ty);
+            return v;
+        }
+
     }
 }
--- 2D RPG ONLAB/Assets/Scripts/Mobs/MageBoss.cs
+++ 2D RPG ONLAB/Assets/Scripts/Mobs/MageBoss.cs
@@ -14,8 +14,24 @@ namespace EventCallbacks
         private bool inMiddle;
         private bool increase;
         private int counter;
-        public float m_CenterX;
-        public float m_CenterY;
+
+        public float m_JumpRadius;
+        public Vector2 m_Middle;
+
+        private Fireball4Ways fireballMaker;
+
+        private int phaseCounter;
+        private int jumpCounter;
+        private int fireBallCounter;
+
+        private void Awake()
+        {
+            this.fireballMaker = gameObject.GetComponent<Fireball4Ways>();
+            
+            phaseCounter = 1;
+            fireBallCounter = 0;
+            jumpCounter = 0;
+        }
 
 
         public override void Attack(Vector2 Dir)
@@ -38,38 +54,95 @@ namespace EventCallbacks
         {
             inMiddle = true;
             increase = true;
-            InvokeRepeating("LaunchProjectile", 2.0f, 0.3f);
+            fireballMaker.startAttacking(false);
+            InvokeRepeating("StartPlaying", 2.0f, 0.5f);
         }
 
-        void LaunchProjectile()
-        {
-            if (inMiddle)
+        private void StartPlaying(){
+
+            switch (phaseCounter)
             {
-                
-                Projectile fb1 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
-                fb1.setDirection(new Vector2(1, 0));
+                case 1:
+                    fireballMaker.startAttacking(true);
+                    fireBallCounter++;
+                    if (fireBallCounter >= 30)
+                    {
+                        fireballMaker.rotateDirection = fireballMaker.rotateDirection * -1;
+                        fireBallCounter = 0;
+                    }
+                    break;
+                case 2:
+                    bool nearplayer = true;
+                    float randomX = gameObject.transform.position.x;
+                    float randomY = gameObject.transform.position.y;
+                    GameObject[] playersToFind = GameObject.FindGameObjectsWithTag("Hero");
 
-                Projectile fb2 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
-                fb2.setDirection(new Vector2(0, 1));
+                    while (nearplayer)
+                    {
+                        randomX = Random.Range(m_Middle.x - m_JumpRadius / 2, m_Middle.x + m_JumpRadius / 2);
+                        randomY = Random.Range(m_Middle.y - m_JumpRadius / 2, m_Middle.y + m_JumpRadius / 2);
+                        bool atLeastOnePlayerNear = false;
+                        for (int i = 0; i < playersToFind.Length; i++)
+                        {
+                            Vector2 playerPos = playersToFind[i].transform.position;
+                            Vector2 currPos = this.transform.position;
+                            float distance = Vector2.Distance(new Vector2(randomX,randomY), playerPos);
+                            if (distance < 2)
+                            {
+                                atLeastOnePlayerNear = true;
+                            }
+                            nearplayer = atLeastOnePlayerNear;
+                        }
+                    }
 
-                Projectile fb3 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
-                fb3.setDirection(new Vector2(-1, 0));
+                    gameObject.transform.position = new Vector3(randomX, randomY, 0);
 
-                Projectile fb4 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
-                fb4.setDirection(new Vector2(0, -1));
+                    Projectile fb1 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
+                    fb1.setDirection(new Vector2(1,0));
 
-                if (increase)
-                {
+                    Projectile fb2 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
+                    fb2.setDirection(new Vector2(0, 1));
 
-                }
+                    Projectile fb3 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
+                    fb3.setDirection(new Vector2(-1, 0));
+
+                    Projectile fb4 = Instantiate(m_EnemyFireBall, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + 90));
+                    fb4.setDirection(new Vector2(0, -1));
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+
+
+        }
+
+
+        private void Update()
+        {
+            if (m_MaxHP * 1/5 > m_HP)
+            {
+                phaseCounter = 5;
+            }
+            else if (m_MaxHP * 2 / 5 > m_HP)
+            {
+                phaseCounter = 4;
+            }
+            else if (m_MaxHP * 3 / 5 > m_HP)
+            {
+                phaseCounter = 3;
+            }
+            else if (m_MaxHP * 4 / 5 > m_HP)
+            {
+                phaseCounter = 2;
             }
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 
 }
